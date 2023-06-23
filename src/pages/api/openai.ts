6e8618d29e088356160be678add72ca39c759c83
samsonc <samsonc0514@gmail.com>
2023-06-23 19:30:26 +0800

    Successfully set up openai key

--- src/pages/api/openai.ts
+++ src/pages/api/openai.ts
@@ -1,36 +1,38 @@
 require("dotenv").config();
 import axios from "axios";
+import { Configuration, OpenAIApi } from "openai";
 
 export async function fetchResponse(prompt: string) {
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+
   try {
-    // const { Configuration, OpenAIApi } = require("openai");
-    // const configuration = new Configuration({
-    //   apiKey: process.env.OPENAI_API_KEY,
-    // });
-    // const openai = new OpenAIApi(configuration);
-    // const response = await openai.createCompletion({
-    //   model: "text-davinci-003",
-    //   prompt: "Say this is a test",
-    //   max_tokens: 7,
-    //   temperature: 0,
-    // });
-    console.log(process.env.OPENAI_API_KEY);
-    const response = await axios.post(
-      "https://api.openai.com/v1/engines/davinci-codex/completions",
-      {
-        prompt,
-        max_tokens: 60,
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-        },
-      }
-    );
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt,
+      max_tokens: 60,
+      temperature: 0.5,
+    });
+    // console.log(process.env.OPENAI_API_KEY);
+    // const response = await axios.post(
+    //   "https://api.openai.com/v1/engines/davinci-codex/completions",
+    //   {
+    //     prompt,
+    //     max_tokens: 60,
+    //   },
+    //   {
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //       Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+    //     },
+    //   }
+    // );
     console.log("OpenAI response:", response.data); // add this line
 
-    return response.data.choices[0].text.trim();
+    return response?.data?.choices[0]?.text?.trim() ?? "";
+    // return response.data['choices'][0]['text'].trim();
   } catch (err) {
     if (axios.isAxiosError(err)) {
       // If the error is an Axios error, you can access its properties
---
+++ testing.ts
@@ -0,0 +1,46 @@
+// const PORT = 8000;
+// const express = require("express");
+// const cors = require("cors");
+// const app = express();
+// app.use(express.json());
+// app.use(cors());
+
+// const API_KEY = "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";
+
+// app.post("/completions", async (req, res) => {
+//   const options = {
+//     method: "POST",
+//     headers: {
+//       Authorization: `Bearer  + ${API_KEY}`,
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({
+//       model: "gpt-3.5-turbo",
+//       messages: [{ role: "user", content: "how are you?" }],
+//       max_tokens: 20,
+//     }),
+//   };
+
+//   try {
+//     // wait for the response from user
+//     const response = await fetch(
+//       "https://api.openai.com/v1/chat/completions",
+//       options
+//     );
+//     const data = await response.json();
+//     res.send(data);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
+
+// app.listen(PORT, () => console.log("server is running" + PORT));
+
+// const readline = require("readline");
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+// rl.question("what are you building: \n", (title) => console.log(title));
