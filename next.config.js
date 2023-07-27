1e2738aaccff315dc53e35024de53d03a9087be8
Nikhil Sharma <nikhilsharma26500@outlook.com>
2023-07-28 01:52:36 +0530

    reverted changes in next config

--- next.config.js
+++ next.config.js
@@ -31,25 +31,17 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  rewrites: async ({ req, resolvedUrl }) => {
-    if (req.headers["x-forwarded-proto"] === "https") {
-      return [
-        {
-          source: "/api/app/:path*",
-          destination:
-            process.env.NODE_ENV === "development"
-              ? "http://127.0.0.1:8000/api/app/:path*"
-              : "https://theguidesai.vercel.app/api/app/:path*",
-        },
-      ];
-    } else {
-      return [
-        {
-          source: "/api/app/:path*",
-          destination: `https://${req.headers.host}${resolvedUrl}`,
-        },
-      ];
-    }
+  rewrites: async () => {
+    return [
+      {
+        source: "/api/app/:path*",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/api/app/:path*"
+            : "https://theguidesai.vercel.app/api/app/:path*",
+        // "/api/app/",
+      },
+    ];
   },
 };
 
