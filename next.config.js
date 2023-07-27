6f21673a4443393c19e97db61d73526442388510
samsonc <samsonc0514@gmail.com>
2023-07-27 16:27:27 +0800

    delete headers in next config

--- next.config.js
+++ next.config.js
@@ -23,6 +23,7 @@ const nextConfig = {
             : "/api/app/",
 >>>>>>> 3c40a92 (added expose_headers)
 
+<<<<<<< HEAD
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
@@ -37,6 +38,21 @@ const nextConfig = {
           },
         ],
 >>>>>>> 0ea5430 (updated next config)
+=======
+        // headers: [
+        //   { key: "Access-Control-Allow-Credentials", value: "true" },
+        //   { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+        //   {
+        //     key: "Access-Control-Allow-Methods",
+        //     value: "GET,DELETE,PATCH,POST,PUT",
+        //   },
+        //   {
+        //     key: "Access-Control-Allow-Headers",
+        //     value:
+        //       "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+        //   },
+        // ],
+>>>>>>> 9c4598f (delete headers in next config)
       },
     ];
   },
