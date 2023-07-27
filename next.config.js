a99eb1db2307ce14e498a39b90f9f33fc0b2c825
samsonc <samsonc0514@gmail.com>
2023-07-27 21:27:09 +0800

    testing config nextjs

--- next.config.js
+++ next.config.js
@@ -90,38 +90,24 @@ const nextConfig = {
             ? "http://127.0.0.1:8000/api/app/:path*"
             : // "http://theguides-git-samson-dev-wetheguide23-gmailcom.vercel.app/api/app/:path*"
               "/api/app/",
-
-        // headers: [
-        //   { key: "Access-Control-Allow-Credentials", value: "true" },
-        //   { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-        //   {
-        //     key: "Access-Control-Allow-Methods",
-        //     value: "GET,DELETE,PATCH,POST,PUT",
-        //   },
-        //   {
-        //     key: "Access-Control-Allow-Headers",
-        //     value:
-        //       "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-        //   },
-        // ],
       },
-      {
-        source: "/api/app/conversation",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,POST,PUT,PATCH,DELETE,OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type,Authorization",
-          },
-        ],
-      },
+      // {
+      //   source: "/api/app/conversation",
+      //   headers: [
+      //     {
+      //       key: "Access-Control-Allow-Origin",
+      //       value: "*",
+      //     },
+      //     {
+      //       key: "Access-Control-Allow-Methods",
+      //       value: "GET,POST,PUT,PATCH,DELETE,OPTIONS",
+      //     },
+      //     {
+      //       key: "Access-Control-Allow-Headers",
+      //       value: "Content-Type,Authorization",
+      //     },
+      //   ],
+      // },
     ];
   },
 };
