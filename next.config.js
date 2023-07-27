fa64fd5772550ea898e555f12eb87aca91c27112
Nikhil Sharma <nikhilsharma26500@outlook.com>
2023-07-28 01:46:37 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -31,18 +31,28 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  rewrites: async () => {
-    return [
-      {
-        source: "/api/app/:path*",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/app/:path*"
-            : "https://theguidesai.vercel.app//api/app/:path*",
-        // "/api/app/",
-      },
-    ];
+  rewrites: async ({ req, resolvedUrl }) => {
+    if (req.headers["x-forwarded-proto"] === "https") {
+      return [
+        {
+          source: "/api/app/:path*",
+          destination:
+            process.env.NODE_ENV === "development"
+              ? "http://127.0.0.1:8000/api/app/:path*"
+              : "https://theguidesai.vercel.app/api/app/:path*",
+        },
+      ];
+    } else {
+      return [
+        {
+          source: "/api/app/:path*",
+          destination: `https://${req.headers.host}${resolvedUrl}`,
+        },
+      ];
+    }
   },
 };
+
+module.exports = nextConfig;
 
 module.exports = nextConfig;
