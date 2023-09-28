df02f8ce0b1819e7598eff22e75b0dd9fe93a072
samsonc <samsonc0514@gmail.com>
2023-09-28 18:32:32 -0400

    done clerk setup

--- src/components/ui/sub-components/Login.tsx
+++ src/components/ui/sub-components/Login.tsx
@@ -1,8 +1,8 @@
 import React from "react";
+import Link from "next/link";
 import Image from "next/image";
 import { useState } from "react";
 import PlaceHolder from "../../../../public/placeholder_userID.png";
-
 
 const Login = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,17 +10,19 @@ const Login = () => {
     <>
       <div className="container mx-auto sm:px-4">
         <div className="backdrop-blur-md backdrop-brightness-50 w-fit h-fit p-2 rounded-3xl absolute right-0 mr-5 mt-5 flex my-auto items-center">
-          <Image
-          src={PlaceHolder}
-          alt="User Image"
-          height={30}
-          width={30}
-          className="rounded-full"
-          />
+          <Link href="sign-in" className="hover:border-white">
+            <Image
+              src={PlaceHolder}
+              alt="User Image"
+              height={30}
+              width={30}
+              className="rounded-full "
+            />
+          </Link>
         </div>
       </div>
     </>
   );
-}
+};
 
-export default Login
+export default Login;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
-
+import { ClerkProvider } from "@clerk/nextjs";
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_FRONTEND_API}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </ClerkProvider>
   );
 }
